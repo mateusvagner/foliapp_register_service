9d5f58748e8e8e38a5a80814ee8d358335ba7206
Mateus Vagner <mateus.vagner@gmail.com>
2022-11-15 17:07:28 -0300

    feat: add files

---
+++ src/main/java/com/foliApp/registerservice/FoliAppRegisterService.java
@@ -0,0 +1,18 @@
+package com.foliApp.registerservice;
+
+import javax.annotation.security.RolesAllowed;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class FoliAppRegisterService {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @RolesAllowed("USER")
+    public String hello() {
+        return "Hello RESTEasy";
+    }
+}
---
+++ src/test/java/com/foliApp/registerservice/FoliAppRegisterServiceIT.java
@@ -0,0 +1,8 @@
+package com.foliApp.registerservice;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class FoliAppRegisterServiceIT extends FoliAppRegisterServiceTest {
+    // Execute the same tests but in packaged mode.
+}
